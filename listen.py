499f35aaf30a2cb6859f8c85b870254189e2f8d9
zteh0010 <zteh0010@student.monash.edu>
2026-08-19 19:30:58 +0800

    Change PWM frequency from 1000Hz to 2000Hz

--- listen.py
+++ listen.py
@@ -62,8 +62,9 @@ def setup_gpio():
     
     # Initialize PWM (frequency: 1000Hz)
     global left_motor_pwm, right_motor_pwm
-    left_motor_pwm = GPIO.PWM(LEFT_MOTOR_ENB, 1000)
-    right_motor_pwm = GPIO.PWM(RIGHT_MOTOR_ENA, 1000)
+    hz=2000
+    left_motor_pwm = GPIO.PWM(LEFT_MOTOR_ENB, hz)
+    right_motor_pwm = GPIO.PWM(RIGHT_MOTOR_ENA, hz)
     left_motor_pwm.start(0)
     right_motor_pwm.start(0)
 
